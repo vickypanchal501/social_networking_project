846cc3507012bc82fad954f6c608c4d58b47d51b
vickypanchal501 <panchalvicky501@gmail.com>
2024-04-03 19:35:37 +0530

    handle Debug setttings

--- social_networking_project/settings/development.py
+++ social_networking_project/settings/development.py
@@ -1,38 +1,7 @@
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
 from .base import *
+DEBUG = True
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-
-# settings.py
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': BASE_DIR /'logger.log',
-            'formatter': 'verbose',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-    'formatters': {
-        'verbose': {
-            'format': '%(asctime)s [%(levelname)s] %(module)s %(message)s'
-        },
-    },
-}
